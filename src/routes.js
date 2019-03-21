05654e2bfa93585b0d315072df763f397ca4b9e0
AlexSmith716 <alexsmith716@outlook.com>
2019-03-21 12:52:35 -0400

    fix(Client): Modified import of createBrowserHistory from library history.

--- src/routes.js
+++ src/routes.js
@@ -4,16 +4,16 @@
 
 import { App, Home, NotFound } from './containers';
 
-// import About from './containers/About/Loadable';
-// import AboutOne from './containers/AboutOne/Loadable';
-// import AboutTwo from './containers/AboutTwo/Loadable';
-// import AboutThree from './containers/AboutThree/Loadable';
-// import AboutFour from './containers/AboutFour/Loadable';
-// import StickyFooter from './containers/StickyFooter/Loadable';
-// import BoardGames from './containers/games/BoardGames/Loadable';
-// import Login from './containers/Login/Loadable';
-// import Register from './containers/Register/Loadable';
-// // import LoginSuccess from './containers/LoginSuccess/Loadable';
+import About from './containers/About/Loadable';
+import AboutOne from './containers/AboutOne/Loadable';
+import AboutTwo from './containers/AboutTwo/Loadable';
+import AboutThree from './containers/AboutThree/Loadable';
+import AboutFour from './containers/AboutFour/Loadable';
+import StickyFooter from './containers/StickyFooter/Loadable';
+import BoardGames from './containers/games/BoardGames/Loadable';
+import Login from './containers/Login/Loadable';
+import Register from './containers/Register/Loadable';
+// import LoginSuccess from './containers/LoginSuccess/Loadable';
 
 // const isAuthenticated = connectedReduxRedirect({
 //   redirectPath: '/login',
@@ -40,17 +40,17 @@ const routes = [{
   component: App,
   routes: [
     { path: '/', exact: true, component: Home },
-    // { path: '/about', component: About },
-    // { path: '/aboutone', component: AboutOne },
-    // { path: '/abouttwo', component: AboutTwo },
-    // { path: '/aboutthree', component: AboutThree },
-    // { path: '/aboutfour', component: AboutFour },
-    // { path: '/stickyfooter', component: StickyFooter },
-    // { path: '/boardgames', component: BoardGames },
-    // { path: '/login', component: Login },
-    // { path: '/register', component: Register },
-    // // { path: '/login-success', component: isAuthenticated(LoginSuccess) },
-    // // { path: '/register', component: isNotAuthenticated(Register) },
+    { path: '/about', component: About },
+    { path: '/aboutone', component: AboutOne },
+    { path: '/abouttwo', component: AboutTwo },
+    { path: '/aboutthree', component: AboutThree },
+    { path: '/aboutfour', component: AboutFour },
+    { path: '/stickyfooter', component: StickyFooter },
+    { path: '/boardgames', component: BoardGames },
+    { path: '/login', component: Login },
+    { path: '/register', component: Register },
+    // { path: '/login-success', component: isAuthenticated(LoginSuccess) },
+    // { path: '/register', component: isNotAuthenticated(Register) },
     { component: NotFound }
   ]
 }];
